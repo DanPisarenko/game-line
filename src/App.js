898d234ca63dcf8234911b8e42baacdca02c0d6e
DanPisarenko <daniil.alex.p@gmail.com>
2024-03-17 21:23:28 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
      <Routes>
         <Route path='/game-line' element={<Main/>}/>
         <Route path='/game-line/games' element={<Games/>}/>
-        <Route path='/game-line/games/:id' element={<GameInfo/>}/>
+        <Route path='/game-line/games/1' element={<GameInfo/>}/>
         {/* <Route path='/rewiew' element={<Review/>}/> */}
-        <Route path='/game-line/review/:id' element={<Review/>}/>
+        <Route path='/game-line/review/1' element={<Review/>}/>
         {/* <Route path='/lr-rewiew/' element={<Games/>}/> */}
         {/* <Route path='/lr-rewiew/:id' element={<Games/>}/> */}
         <Route path='*' element={<h2>Страница не найдена</h2>}/>
