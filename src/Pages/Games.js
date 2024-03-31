291190f884663bca0bc19c296bbb3d7874a33d57
DanPisarenko <daniil.alex.p@gmail.com>
2024-03-31 21:38:25 +0300

    create project

--- src/Pages/Games.js
+++ src/Pages/Games.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import '../gamesList.css'
+import axios from "axios"
 
 export default function Games(){
 
@@ -50,19 +51,34 @@ export default function Games(){
         'rating': 5
     }]
 
+    const [games, setGames] = useState([])
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/game`)
+            .then(res => {
+                console.log(res.data.rows)
+                setGames(res.data.rows)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    
+}, []);
+
+
     return(
         <div className="games">
             <div className="games__list-block">
                 <div className="games__list-block_list">
-                    {base.map((e) =>{
+                    {games.map((e) =>{
                         return(
-                            <div className="games__list-block_list_item" key={e.id} onClick={(e) => window.location.href="/games/1"}>
-                            <div className="games__list-block_list_itme_image" style={{backgroundImage: `url("https://www.digiseller.ru/preview/928496/p1_3740137_6c8e439a.jpg")`}}>
+                            <div className="games__list-block_list_item" key={e.id} onClick={(e) => window.location.href=`/games/${e.id}`}>
+                            <div className="games__list-block_list_itme_image" style={{backgroundImage: `url(${e.image})`}}>
                          </div>
                             <div className="games__list-block_list_item_info">
-                                <h3 className="games__list-block_list_item_info_name">{e.game_name}</h3>
-                                <p className="games__list-block_list_item_info_txt">Жанр: {e.jenre}</p>
-                                <p className="games__list-block_list_item_info_txt">Год выхода: {e.year_anonce}</p>
+                                <h3 className="games__list-block_list_item_info_name">{e.name}</h3>
+                                <p className="games__list-block_list_item_info_txt">Жанр: </p>
+                                <p className="games__list-block_list_item_info_txt">Год выхода: {e.year_of_pub}</p>
                                 <p className="games__list-block_list_item_info_txt">Год выхода: 2023 год</p>
                             </div>
                         </div>
