c59909b014229aa3899041ccd2c7e0873252008b
DanPisarenko <daniil.alex.p@gmail.com>
2024-04-11 21:28:13 +0300

    create project

--- src/Pages/HelperLinePage.js
+++ src/Pages/HelperLinePage.js
@@ -1,22 +1,63 @@
 import '../App.css'
+import { useState, useEffect } from 'react';
+import {useParams} from 'react-router'
+import  axios  from 'axios';
 export default function HelperLinePage(){
 
+    
+    const {id} = useParams()
+    const [game, setGame] = useState([])
+    const [answer, setAnswer] = useState([])
+    const [idAnswer, setIdAnswer] = useState('')
 
+    useEffect(() => {
+        if ( id ) {
+            axios.get(`http://localhost:8080/api/qa/${id}`)
+                .then(res => {
+                    console.log(res.data.rows)
+                    setGame(res.data.rows)
+                    setIdAnswer(res.data.rows[0].id)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }, [ id ]);
+    useEffect(() => {
+        if ( idAnswer ) {
+            axios.get(`http://localhost:8080/api/qa/answer/${idAnswer}`)
+                .then(res => {
+                    console.log(res.data.rows)
+                    setAnswer(res.data.rows)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }, [ idAnswer ]);
 
     return(
         <div className="helper_line_page">
-            <div className='helper_line_page_quest'>
-                <h4 className="helper_line__best_qa_item_theme">Автор: John1337</h4>
-                <h4 className="helper_line__best_qa_item_theme">Старые игры</h4>
-                <h3 className="helper_line__best_qa_item_title">Что лучше эмулятор старого ПК или реальный ретро-ПК</h3>
-            </div>
+            {game.map((e) => {
+                return(
+                <div className='helper_line_page_quest'>
+                    <h4 className="helper_line__best_qa_item_theme">Автор: John1337</h4>
+                    <h4 className="helper_line__best_qa_item_theme">{e.theme}</h4>
+                    <h3 className="helper_line__best_qa_item_title">{e.question}</h3>
+                </div>                  
+                )
+            })}
             <div className='helper_line_page_quest_answers'>
                 <h2 className='helper_line_page_quest_answers_title'>Ответы</h2>
                 <div className='helper_line_page_quest_answers_list'>
-                    <div className='helper_line_page_quest_answers_item'>
-                        <h4 className="helper_line__best_qa_item_theme">Автор: Alex98</h4>
-                        <p className="helper_line__best_qa_item_title">По поводу эмуляторов, на некоторых компьютерах даные программы могут барахлить из-за характеристик реального ПК, на реальном ретро-ПК может по разному, некоторые комплектующие могут выйти из строя, в основном жесткий диск может выйти из строя</p>
-                    </div>
+                    {answer.map((e) => {
+                        return(
+                        <div className='helper_line_page_quest_answers_item'>
+                            <h4 className="helper_line__best_qa_item_theme">Автор: Alex98</h4>
+                            <p className="helper_line__best_qa_item_title">{e.answer}</p>
+                        </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
